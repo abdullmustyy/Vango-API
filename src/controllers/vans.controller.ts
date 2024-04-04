31803f2bb9d3d25c5c9db1a173ce0c3d508c248d
abdullmustyy <abdulmusty03@outlook.com>
2024-04-04 17:10:23 +0100

    feat: implement host van detail endpoint

--- src/controllers/vans.controller.ts
+++ src/controllers/vans.controller.ts
@@ -5,38 +5,96 @@ import { ResponseHandler } from "../utils/response.utils";
 
 const { vans } = new PrismaClient();
 
+// Get all vans
 export const getAllVans: RequestHandler = async (req, res) => {
   try {
     const allVans = await vans.findMany();
 
-    ResponseHandler.success(res, allVans, 200, "All vans fetched successfully");
+    ResponseHandler.success(
+      res,
+      allVans,
+      200,
+      "All vans fetched successfully."
+    );
   } catch (error) {
     console.error("Error: ", error);
-    throw new InternalServerError("Something went wrong");
+    throw new InternalServerError("Something went wrong.");
   }
 };
 
+// Get a van by id
 export const getVan: RequestHandler = async (req, res) => {
   try {
     const { id } = req.params;
-    
+
     const van = await vans.findUnique({
       where: {
-        id
+        id,
       },
     });
 
     if (!van) {
-      return new NotFoundError("Van not found");
+      return new NotFoundError("Van not found.");
     }
 
-    ResponseHandler.success(res, van, 200, "Van fetched successfully");
+    ResponseHandler.success(res, van, 200, "Van fetched successfully.");
   } catch (error) {
     console.error("Error: ", error);
     throw new InternalServerError("Something went wrong");
   }
 };
 
+// Get vans by host id
+export const getHostVans: RequestHandler = async (req, res) => {
+  try {
+    const { hostId } = req.params;
+
+    const hostVans = await vans.findMany({
+      where: {
+        hostId,
+      },
+    });
+
+    if (!hostVans) {
+      return new NotFoundError("Vans not found for this host.");
+    }
+
+    ResponseHandler.success(
+      res,
+      hostVans,
+      200,
+      "Host vans fetched successfully"
+    );
+  } catch (error) {
+    console.error("Error: ", error);
+    throw new InternalServerError("Something went wrong.");
+  }
+};
+
+// Get a van by host id
+export const getHostVan: RequestHandler = async (req, res) => {
+  try {
+    const { hostId, vanId } = req.params;
+
+    const hostVan = await vans.findFirst({
+      where: {
+        id: vanId,
+        hostId,
+      },
+    });
+
+    if (!hostVan) {
+      return new NotFoundError("Van not found for this host.");
+    }
+
+    ResponseHandler.success(res, hostVan, 200, "Host van fetched successfully");
+  } catch (error) {
+    console.error("Error: ", error);
+    throw new InternalServerError("Something went wrong.");
+  }
+};
+
+// Create a van
 export const createVan: RequestHandler = async (req, res) => {
   try {
     const { name, description, price, type, imageUrl, hostId } = req.body;
@@ -51,9 +109,9 @@ export const createVan: RequestHandler = async (req, res) => {
       },
     });
 
-    ResponseHandler.success(res, newVan, 201, "Van created successfully");
+    ResponseHandler.success(res, newVan, 201, "Van created successfully.");
   } catch (error) {
     console.error("Error: ", error);
-    throw new InternalServerError("Something went wrong");
+    throw new InternalServerError("Something went wrong.");
   }
 };
--- src/routes/vans.route.ts
+++ src/routes/vans.route.ts
@@ -1,12 +1,22 @@
 import { Router } from "express";
 // Controllers
-import { getAllVans, createVan, getVan } from "../controllers/vans.controller";
+import {
+  getAllVans,
+  createVan,
+  getVan,
+  getHostVans,
+  getHostVan,
+} from "../controllers/vans.controller";
 
 const vansRouter = Router();
 // Get all vans route
 vansRouter.get("/vans", getAllVans);
 // Get a van route
 vansRouter.get("/vans/:id", getVan);
+// Get vans by host id route
+vansRouter.get("/vans/host/:hostId", getHostVans);
+// Get a van by host id and van id route
+vansRouter.get("/vans/host/:hostId/:vanId", getHostVan);
 // Create a van route
 vansRouter.post("/vans/create", createVan);
 
